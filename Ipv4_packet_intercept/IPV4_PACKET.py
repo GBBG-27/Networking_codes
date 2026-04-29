a8a1f6681c67fe2eb4a070112f48e47f0ce041c4
Gabriel_Boateng <gabbyboatisthegoatnum1@gmail.com>
2026-04-29 03:00:42 -0400

    Refactor packet handling and interface selection

--- Ipv4_packet_intercept/IPV4_PACKET.py
+++ Ipv4_packet_intercept/IPV4_PACKET.py
@@ -1,65 +1,81 @@
 #!/bin/python3
-
 import socket
 
 
 def packet_socket(Interface , ether_type):
-      
-      try:
-          inter = Interface
-          frame_type = ether_type
-          
-          sock = socket.socket(socket.AF_PACKET , socket.SOCK_RAW)
-          sock.bind((inter , frame_type))
-      
-          return sock
-      except KeyboardInterrupt:
-          print('\n')
-      except Exception as exc:
-          print(exc)
-       
-def recv_send():
-    try:
-     
-      if_index = socket.if_nameindex()
-            
-      for id_if in if_index:
-           
-           Interface_database = {} 
-           
-           ID = id_if[0]
-           IF = id_if[1]
-           
-           Interface_database[f'Index {ID}'] = f'Interface {IF}'
-           
-           if IF.startswith('e'):
-              Interface_name = IF
-              Ether_type = socket.ETHERTYPE_IP
-              
-              sock = packet_socket(Interface_name , Ether_type)
-              if sock is not None:
-                 with open('RECEIVING_IPV4_PACKET.txt' , 'wb') as recv_file:
-                      
+         try: 
+               inter = Interface
+               frame_type = ether_type
 
-                         recv_file.write(b'IPv4 Packet\n')
-                         recv_file.write(b"f''{'*' * 15}''" )
-                         recv_file.write(b'\n')
-                         
-                         while True:
-                                    raw_data = sock.recv(1024)
-                                    decoded_data = raw_data.decode(errors='ignore')
-                                    n_bytes = recv_file.write(raw_data)          
-                                    print(f'Sent [{n_bytes}] bytes to file')
+               sock = socket.socket(socket.AF_PACKET , socket.SOCK_RAW)
+               sock.bind((inter , frame_type))
            
-    except KeyboardInterrupt:
-                 print('\n')
-    except Exception as exc:
-                 print(exc) 
+               return sock
+         except Exception as exc:
+                  print(f'Error_A : {exc}')      
+        
+def recv_write():
+
+            interface_name_index = socket.if_nameindex()
+
+            interface_store = {}
+            index_store = []
+
+            print('Available Interface')
+            print(f"{'`' * 19}")
+
+            for inters in interface_name_index:
+
+                  inter_index = inters[0]
+                  inter_name = inters[1]
+                  interface_store[inter_index] = inter_name
+
+                  print(f">>> [{inter_index}]:[{inter_name}]")
+
+            for num in interface_store:    
+                   index_store.append(num)
 
 
-recv_send() 
-      
-      
-      
-      
+            print("\nInteface")
+            print(f'{"`" * 8}')
+            #print(interface_store)
+            #print(index_store)
 
+            try:
+                   answer = int(input(">>> "))
+            except ValueError:
+                   answer = None
+            except KeyboardInterrupt:
+                   answer = None
+                   
+            try: 
+              if answer in index_store and answer <= len(index_store):
+
+                interface = interface_store[answer]
+                ether_type = socket.ETHERTYPE_IP 
+                sock = packet_socket(interface , ether_type)
+
+                with open("RECEIVING_IPV4_PACKET.txt" , 'a') as packet_file:
+
+                        packet_file.write('IPv4 data\n')
+                        packet_file.write(f"{'*' * 11}\n")
+
+                        if sock is not None:
+                               print('\n')
+                               while True:
+                                      try:
+                                          recv_data = sock.recv(1024)
+                                          decode_data = recv_data.decode(errors='ignore')
+                                          n_bytes = packet_file.write(decode_data)
+                                          print(f"Sent [{n_bytes}] to file")
+                                      except KeyboardInterrupt:
+                                          break     
+                        else:
+                              pass    
+              else:
+               print('\nInvalid answer')                      
+
+            except Exception as exc:
+               print(f'Error_B: {exc}')    
+
+recv_write()
