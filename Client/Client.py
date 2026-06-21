624834fb6525e693161ff491345297020644fd4b
Gabriel_Boateng <gabbyboatisthegoatnum1@gmail.com>
2026-06-21 17:17:19 +0000

    Update Client.py

--- Client/Client.py
+++ Client/Client.py
@@ -1,51 +1,53 @@
 #!/bin/python3
 import socket
+# This is a simple python script acting as a client that 
+# connects to servers.You will be prompt to enter a hostname
+# which could be an ip address or domain name,then a port number,
+# the script will connect to the address and wait for you to 
+# send data
 
 
+def client_socket(): # function to return the socket object 
+             try: # catching errors and exception
 
+                    host = input("Hostname: ") # hostname
+                    port = int(input("Port: ")) # port 
 
-def client_socket(): 
-             try:
-
-                    host = input("Hostname: ")
-                    port = int(input("Port: "))
-
-                    client_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-                    client_socket.connect(( host , port ))
-                    client_socket
+                    client_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM) # socket object
+                    client_socket.connect(( host , port )) # connect to address
               
-                    return client_socket
+                    return client_socket # return the socket object
              
-             except Exception as exc:
+             except Exception as exc: # catching exception
                     
                     return None
               
 
-def process():  
-            try:
+def process(): # function to send and receive data
+            try: # catching errors and exception
              
-                    sock = client_socket()
+                    sock = client_socket() # the socket object 
                     
-                    if sock:
-                     
-                             while True:
+                    if sock: # cross-check if socket object was obtained
+                             print('\n[ Connected ]\r')
+                             while True: # loop to send and receive data
                       
-                                     data = input("\n>> ")
-                                     if len(data) != 0: 
+                                     data = input("\n>> ") # data to be sent
+                                     if len(data) != 0: # check whether length of data is not zero
                                      
-                                             encode_data = data.encode()
-                                             sock.send(encode_data)
-                                     else:
+                                             encode_data = data.encode() # encode data
+                                             sock.send(encode_data) # send data
+                                     else: # executes if length of data is zero
                                              print('\n[ Improper request ]')
                                              break
                                              
                                              
-                                     recv_data = sock.recv(1024)
+                                     recv_data = sock.recv(1024) # receive data
                                      
-                                     if len(recv_data) != 0:
+                                     if len(recv_data) != 0: # check whether received data is not zero 
                                           
-                                            decode_recv_data = recv_data.decode()
-                                            print(f"Responds: {decode_recv_data}")
+                                            decode_recv_data = recv_data.decode() # decode received data
+                                            print(f"Responds: {decode_recv_data}") # print received data out 
                                      
                                      else:
                                            print("\n[ Server disconnected]")
@@ -54,20 +56,20 @@ def process():
                            
                            print('\n[Connection failed]')
             
-            except BrokenPipeError:
+            except BrokenPipeError: # catch brokenpipeerror
                     
                     print("\n[ Server disconnected ]")
                            
-            except Exception as exc:
+            except Exception as exc: catch exceptions
                     
                     print(exc)
              
-            except KeyboardInterrupt:
+            except KeyboardInterrupt: catch keyboardinterrupt
                     ...              
                            
                            
                            
-process()
+process() # run the function
 
 
 
